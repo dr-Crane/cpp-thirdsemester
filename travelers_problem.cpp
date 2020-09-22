6d2283336b39e741e637ad2509ec8b5392b4a996
Bekzat <61666791+dr-Crane@users.noreply.github.com>
2020-09-22 00:59:30 -0400

    Update travelers_problem.cpp

--- travelers_problem.cpp
+++ travelers_problem.cpp
@@ -18,6 +18,10 @@ public:
     route(const route &obj)
     {
         amount = obj.amount;
+        if(trip!=NULL)
+        {
+            delete[] trip;
+        }
         trip = new int [amount];
         for(int i=0; i<amount; i++)
         {
@@ -51,6 +55,7 @@ public:
     }
 
     friend ostream &operator << (ostream &os, const route &obj);
+    friend int get_cost(int **matrix, int amount, route obj);
 };
 
 ostream &operator << (ostream &os, const route &obj)
@@ -117,14 +122,42 @@ void show_all_shifts (route *variants, int amount)
 
 }
 
+int get_cost(int **matrix, int amount, route obj)
+{
+    int result = 0;
+    int *trip = new int [amount];
+    for(int i=0; i<amount; i++)
+    {
+        trip[i] = obj.trip[i];
+    }
+    int i=0, j=0, m=0;
+    while(m<amount)
+    {
+        i = trip[m];
+        j = trip[m+1];
+        result += matrix[i][j];
+        m++;
+    }
+    result += matrix[j][0];
+
+    return result;
+}
+
 int main()
 {
     int amount=0;
     int range=1;
 
-    cout<<"Amount of the cities:"<<endl;
+    cout<<"Amount of the cities: ";
     cin>>amount;
     if((amount==0)||(amount==1)) return 1;
+
+
+    int **matrix = new int* [amount];
+    for(int i=0; i<amount; i++)
+    {
+        matrix[i] = new int [amount];
+    }
 
     for(int i=1; i<amount; i++)
     {
@@ -136,15 +169,61 @@ int main()
     {
         variants[i] = amount;
     }
-    
-    show_all_shifts(variants, amount);
 
 
-    for(int i=0; i<range; i++)
+    cout<<"Costs of trips:"<<endl;
+    int space=0, m=0, n=0;
+    while(m<amount)
     {
-        cout<<variants[i]<<endl;
+        n = 0;
+        while(n<amount)
+        {
+            if(m==n)
+            {
+                matrix[m][n]=0;
+                n++;
+                continue;
+            }
+            cout<<m<<"->"<<n<<"  = ";
+            cin>>space;
+            matrix[m][n] = space;
+            n++;
+        }
+        m++;
     }
 
+    for(int i=0; i<amount; i++)
+    {
+        for(int j=0; j<amount; j++)
+        {
+            cout<<matrix[i][j]<<"\t";
+        }
+        cout<<endl;
+    }
+
+    show_all_shifts(variants, amount);
+
+    int cost = 0;
+    route lowest = variants[0];
+    int num = get_cost(matrix, amount, variants[0]);
+    for(int i=0; i<range; i++)
+    {
+        cout<<variants[i]<<"  =   ";
+        cost = get_cost(matrix, amount, variants[i]);
+        if(cost<num)
+        {
+            num = cost;
+            lowest = variants[i];
+        }
+        cout<<cost<<endl;
+    }
+
+    cout<<"\n"<<"optimal trip is: "<<lowest<<"  cost  =   "<<num<<endl;
+
     delete[] variants;
+    for(int i=0; i<amount; i++)
+    {
+        delete[] matrix[i];
+    }
     return 0;
 }
