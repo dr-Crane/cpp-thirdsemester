72658e2c83f1296a4a83acfe8b748876d709e649
Bekzat <61666791+dr-Crane@users.noreply.github.com>
2020-09-30 06:31:55 -0400

    Update travelers_problem.cpp

--- travelers_problem.cpp
+++ travelers_problem.cpp
@@ -1,205 +1,196 @@
 #include <iostream>
 #include <cstring>
 
-using std::cout;
-using std::cin;
-using std::ostream;
-using std::endl;
+using namespace std;
 
-class route
+class String
 {
-    int amount;
-    int *trip;
+    char *line;
+    int len;
 public:
-    route(int a=0)
-    {
-        amount = a;
-        trip = new int [amount];
-        for(int i=0; i<amount; i++)
-        {
-            trip[i] = i;
-        }
-    }
-    route(const route &obj)
-    {
-        amount = obj.amount;
-        trip = new int [amount];
-        for(int i=0; i<amount; i++)
-        {
-            trip[i] = obj.trip[i];
-        }
-    }
-   ~route()
-    {
-        delete[] trip;
-    }
-    void make_shift(int amount);
-    int get_cost(int **matrix, int amount);
-    void operator = (const route &obj)
-    {
-        amount = obj.amount;
-        trip = new int [amount];
-        for(int i=0; i<amount; i++)
-        {
-            trip[i] = obj.trip[i];
-        }
-    }
+    String();
+    String(char *arr);
+    String(const String &obj);
+   ~String();
+    
+    char &operator [] (int pos);
+    String &operator + (String &obj);
+    String &operator = (String &obj);
 
+    int *table();
+    int bm_search(String word);
+    int kmp_search (String &obj);
 
-    friend ostream &operator << (ostream &os, const route &obj);
+    friend ostream & operator << (ostream &os, const String &obj);
 };
 
-ostream &operator << (ostream &os, const route &obj)
+ostream & operator << (ostream &os, const String &obj)
 {
-    for(int i=0; i<obj.amount; i++)
+    for(int i=0; i<obj.len; i++)
     {
-        os<<obj.trip[i];
-        if((obj.amount-1)==i) break;
-        os<<"->";
+        os<<obj.line[i];
     }
-    os<<"->0";
     return os;
 }
 
-void route::make_shift(int amount)
+String :: String()
 {
+    line = nullptr;
+    len = 0;
+}
+
+String :: String(char *arr)
+{
+    int arr_len = strlen(arr);
+    len = arr_len;
+    line = new char [len+1]; // may be error cause len+1 }} change to len
+    line[len] = '\n';
+    for(int i=0; i<len; i++)
+    {
+        line[i] = arr[i];
+    }
+}
+
+String ::~String()
+{
+    delete[] line;
+}
+
+String :: String(const String &obj)
+{
+    len = obj.len;
+    line = new char [len+1];
+    line[len] = '\n';
+    for(int i=0; i<len; i++)
+    {
+        line[i] = obj.line[i];
+    }
+}
+
+char& String ::operator [] (int pos)
+{
+    return line[pos];
+}
+
+String& String :: operator + (String &obj)
+{
+    char *space = new char [len];
+    for(int i=0; i<len; i++)
+    {
+        space[i] = line[i];
+    }
+    int new_len = len+obj.len;
+    delete[] line;
+    line = new char [new_len];
+    line[new_len] = '\n';
+    for(int i=0; i<len; i++)
+    {
+        line[i] = space[i];
+    }
+    int i=len, j=0;
+    while(i<new_len)
+    {
+        line[i] = obj.line[j];
+        i++;
+        j++;
+    }
+    len = new_len;
+
+    delete[] space;
+    return *this;
+}
+
+String& String :: operator = (String &obj)
+{
+    delete[] line;
+    len = obj.len;
+    line = new char [len+1];
+    line[len] = '\n';
+    for(int i=0; i<len; i++)
+    {
+        line[i] = obj.line[i];
+    }
+    return *this;
+}
+
+int* String :: table() 
+{
+    int *mass= new int [len];
+
+    for(int i=1; i<len; i++)
+    {
+        mass[len-i-1]=i;
+        for(int j=len-i; j<len-1; j++)
+        {
+            if(line[len-i-1]==line[j] && i!=1) mass[len-i-1]=mass[len-i];
+        }
+    }
+    for(int i=0; i<len-1; i++)
+    {
+        if(line[i]==line[len-1])
+        {
+            mass[len-1]=mass[i];
+            break;
+        }
+        else mass[len-1]=1 + mass[0];
+    }
+
+    return mass;
+}
+
+int String :: bm_search(String word)
+{
+    int *tablew = word.table();
     int space = 0;
-    
-    int i = amount-2;
-    int j = amount-1;
-    while(i!=0)
+    for(int i=word.len-1; i<=len; )
     {
-        if(trip[i]<trip[i+1]) break;
-        i--;
-    }
-    while(i<j)
-    {
-        if(trip[i]<trip[j]) break;
-        j--;
-    }
-    space = trip[i];
-    trip[i] = trip[j];
-    trip[j] = space;
-    int low = i+1;
-    int high = amount-1;
-    while(low<high)
-    {
-        space = trip[low];
-        trip[low] = trip[high];
-        trip[high] = space;
-        low++;
-        high--;
-    }
-}
-
-int route:: get_cost(int **matrix, int amount)
-{
-    int result = 0;
-    int i=0, j=0, m=0;
-    while(m<amount)
-    {
-        i = trip[m];
-        j = trip[m+1];
-        result += matrix[i][j];
-        m++;
-    }
-    result += matrix[j][0];
-
-    return result;
-}
-
-int **make_matrix(int amount)
-{
-    int **matrix = new int* [amount];
-    for(int i=0; i<amount; i++)
-    {
-        matrix[i] = new int [amount];
-    }
-    return matrix;
-}
-
-void fill_matrix(int **matrix, int amount)
-{
-    cout<<"Costs of trips:"<<endl;
-    int space=0, m=0, n=0;
-    while(m<amount)
-    {
-        n = 0;
-        while(n<amount)
-        {
-            if(m==n)
+        for(int j=1; j<word.len; j++){
+            if(line[i-j+1]!=word[word.len-j])
             {
-                matrix[m][n]=0;
-                n++;
-                continue;
+                int t=0;
+                for(int k=0; k<word.len; k++)
+                {
+                    if(line[i-j+1]==word[k])
+                    {
+                        t = tablew[k];
+                        break;
+                    }
+                    else t=0;
+                }
+                if(t) 
+                {
+                    i = i + t;
+                    break;
+                }
+                else i = i+word.len-1;
+                break;
             }
-            cout<<m<<"->"<<n<<"  = ";
-            cin>>space;
-            matrix[m][n] = space;
-            n++;
+            else if((word.len-j-1)==0)
+            {
+                space = i-4;
+                return space;
+            }
         }
-        m++;
     }
+    return space;
 }
 
-void print_matrix(int **matrix, int amount)
+int main()
 {
-    for(int i=0; i<amount; i++)
-    {
-        for(int j=0; j<amount; j++)
-        {
-            cout<<matrix[i][j]<<"\t";
-        }
-        cout<<endl;
-    }
-}
+    
+    char a[] = "na dwore drowa, na trowe drowa";
+    char b[] = "drowa";
 
-void true_solve(int amount)
-{
-    int **matrix = make_matrix(amount);
-    fill_matrix(matrix, amount);
-    print_matrix(matrix, amount);
-    int range =1;
-    for(int i=1; i<amount; i++)
-    {
-        range = range*i;
-    }
+    String arr_1 = a;
+    String arr_2 = b;
 
-    route lowest = amount;
-    route space = amount;
-
-    int cost = lowest.get_cost(matrix, amount);
-    int num = 0;
-    cout<<space<<"   = "<<cost<<endl;
-
-    for(int i=0; i<range-1; i++)
-    {
-        space.make_shift(amount);
-        num = space.get_cost(matrix, amount);
-        cout<<space<<"   = "<<num<<endl;
-        if(num<cost)
-        {
-            cost = num;
-            lowest = space;
-        }
-       
-    }
-
-    cout<<"\n\n"<<"Optimal trip:\n"<<lowest<<"   = "<<cost<<endl;
-
-    delete[] matrix;
-}
-
-int main ()
-{
-    int amount=0;
-
-    cout<<"Amount of the cities: ";
-    cin>>amount;
-    if((amount==0)||(amount==1)||(amount==2)) return 1;
-
-    true_solve(amount);
-
+//  Examole for bm_search();
+    // int pos = arr_1.bm_search(arr_2);
+    // for(int i=0; i<strlen(b); i++)
+    // {
+    //     arr_1[pos+i] = ' ';
+    // }
+    // cout<<arr_1<<endl;
 
 
     return 0;
