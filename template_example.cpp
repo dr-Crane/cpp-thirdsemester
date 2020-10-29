9ec615fd9a3002fa4cbab9f69ca5e7b3746a1bef
Bekzat <61666791+dr-Crane@users.noreply.github.com>
2020-10-29 11:37:42 +0700

    Create template_example.cpp

---
+++ template_example.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+#include <iomanip>
+ 
+template <typename T>
+class Stack
+{
+private:
+    T *stackPtr; // указатель на стек
+    int size; // размер стека
+    T top; // вершина стека
+public:
+    Stack(int = 10);// по умолчанию размер стека равен 10 элементам
+    ~Stack(); // деструктор
+    bool push(const T  ); // поместить элемент в стек
+    bool pop(); // удалить из стека элемент
+    void printStack();
+};
+ 
+ // конструктор
+template <typename T>
+Stack<T>::Stack(int s)
+{
+    size = s > 0 ? s: 10;   // инициализировать размер стека
+    stackPtr = new T[size]; // выделить память под стек
+    top = -1; // значение -1 говорит о том, что стек пуст
+}
+ 
+// деструктор
+template <typename T>
+Stack<T>::~Stack()
+{
+    delete [] stackPtr; // удаляем стек
+}
+ 
+// элемент функция класса  Stack для помещения элемента в стек
+// возвращаемое значение - true, операция успешно завершена
+//                                    false, элемент в стек не добавлен
+template <typename T>
+bool Stack<T>::push(const T value)
+{
+    if (top == size - 1)
+        return false; // стек полон
+ 
+    top++;
+    stackPtr[top] = value; // помещаем элемент в стек
+ 
+    return true; // успешное выполнение операции
+}
+ 
+// элемент функция класса  Stack для удаления элемента из стек
+// возвращаемое значение - true, операция успешно завершена
+//                                    false, стек пуст
+template <typename T>
+bool Stack<T>::pop()
+{
+    if (top == - 1)
+        return false; // стек пуст
+ 
+    stackPtr[top] = 0; // удаляем элемент из стека
+    top--;
+ 
+    return true; // успешное выполнение операции
+}
+int main()
+{
+    Stack <int> myStack(5);
+ 
+    // заполняем стек
+    cout << "Заталкиваем элементы в стек: ";
+    int ct = 0;
+    while (ct++ != 5)
+    {
+        int temp;
+        cin >> temp;
+        myStack.push(temp);
+    }
+ 
+    myStack.printStack(); // вывод стека на экран
+ 
+    cout << "\nУдаляем два элемента из стека:\n";
+ 
+    myStack.pop(); // удаляем элемент из стека
+    myStack.pop(); // удаляем элемент из стека
+    myStack.printStack(); // вывод стека на экран
+ 
+    return 0;
+}
+ 
+
+ 
