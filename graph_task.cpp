ccdf308d7a434e89ea28ac5e12471d652f231534
Bekzat <61666791+dr-Crane@users.noreply.github.com>
2020-12-16 00:27:23 -0500

    Update graph_task.cpp

--- graph_task.cpp
+++ graph_task.cpp
@@ -32,47 +32,47 @@ bool circle_exist (leader *vx, leader *vy, leader *vz)
     leader *ptrVY = vy;
     leader *ptrVZ = vz;
 
-    if(ptrVX->st==0&&ptrVY->st==0&&ptrVZ->st==0)
+    if(ptrVX->st==0||ptrVY->st==0||ptrVZ->st==0)
     {
         return false;
     }
 
     leader *tmpVX = ptrVX;
 
-    while(tmpVX)
+
+    trailer *list_1 = tmpVX->list;
+    
+    while(list_1)
     {
-        trailer *list_1 = tmpVX->list;
-        while(list_1)
+        if(list_1->vert == ptrVY || list_1->vert == ptrVZ)
         {
-            if(list_1->vert == ptrVY || list_1->vert == ptrVZ)
+            leader *tmpVY = list_1->vert;
+            trailer *list_2 = tmpVY->list;
+            while(list_2)
             {
-                leader *tmpVY = list_1->vert;
-                trailer *list_2 = tmpVY->list;
-                while(list_2)
+                if(list_2->vert == ptrVX)
                 {
-                    if(list_2->vert == ptrVX)
-                    {
-                        return true;
-                    }
-                    if(list_2->vert == ptrVZ)
-                    {
-                        leader *tmpVZ = ptrVZ;
-                        trailer *list_3 = tmpVZ->list;
-                        while(list_3)
-                        {
-                            if(list_3->vert==ptrVX)
-                            {
-                                return true;
-                            }
-                            list_3 = list_3->next;
-                        }
-                    }
-                    list_2 = list_2->next;
+                    return true;
                 }
+                if(list_2->vert == ptrVZ)
+                {
+                    leader *tmpVZ = ptrVZ;
+                    trailer *list_3 = tmpVZ->list;
+                    while(list_3)
+                    {
+                        if(list_3->vert==ptrVX)
+                        {
+                            return true;
+                        }
+                        list_3 = list_3->next;
+                    }
+                }
+                    ist_2 = list_2->next;
             }
-            list_1 = list_1->next;
         }
+        list_1 = list_1->next;
     }
+
 
     return false;
 
