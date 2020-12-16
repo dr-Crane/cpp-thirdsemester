9f3d76382e79de3500cdde7ceba55b96860d9a69
Bekzat <61666791+dr-Crane@users.noreply.github.com>
2020-12-16 00:21:32 -0500

    Update graph_task.cpp

--- graph_task.cpp
+++ graph_task.cpp
@@ -32,19 +32,46 @@ bool circle_exist (leader *vx, leader *vy, leader *vz)
     leader *ptrVY = vy;
     leader *ptrVZ = vz;
 
-    if(ptrVX->st==0&&ptrVY->st==0&&ptrVZ->st==0)
+    if(ptrVX->st==0||ptrVY->st==0||ptrVZ->st==0)
     {
         return false;
     }
 
-    leader *tmp = ptrVX;
-    if(tmp->list->vert->list->vert == ptrVX) return true;
-    if(tmp->list->vert->list->vert->list->vert == ptrVX) return true;
+    leader *tmpVX = ptrVX;
 
-    if(tmp->list->vert->st==2)
+    while(tmpVX)
     {
-        if(tmp->list->next->vert == ptrVX) return true;
-        if(tmp->list->next->vert->list->vert == ptrVX) return true;
+        trailer *list_1 = tmpVX->list;
+        while(list_1)
+        {
+            if(list_1->vert == ptrVY || list_1->vert == ptrVZ)
+            {
+                leader *tmpVY = list_1->vert;
+                trailer *list_2 = tmpVY->list;
+                while(list_2)
+                {
+                    if(list_2->vert == ptrVX)
+                    {
+                        return true;
+                    }
+                    if(list_2->vert == ptrVZ)
+                    {
+                        leader *tmpVZ = ptrVZ;
+                        trailer *list_3 = tmpVZ->list;
+                        while(list_3)
+                        {
+                            if(list_3->vert==ptrVX)
+                            {
+                                return true;
+                            }
+                            list_3 = list_3->next;
+                        }
+                    }
+                    list_2 = list_2->next;
+                }
+            }
+            list_1 = list_1->next;
+        }
     }
 
     return false;
